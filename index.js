d2908820fcad824875c99ed2cc1429eea7cb3c98
gosrahul21 <gosrahul21@gmail.com>
2021-10-11 19:01:44 +0530

    error in uploading

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const eventEmitter = require('./EventEmitter')
 const app = express();
 const port = process.env.PORT 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+// app.use(express.urlencoded({limit: '50mb'}));
 app.use(require('cors')());
 app.use(express.json());
 app.use('/user',require('./router/User'));
